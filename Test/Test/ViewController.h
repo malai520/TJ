8e528d4294ffe7a5e4b123113ec4ec4b5a553cdf
malai520 <malai_520@163.com>
2015-01-23 11:17:02 +0800

    test

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by TJ on 15-1-13.
+//  Copyright (c) 2015年 LiCQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by TJ on 15-1-13.
+//  Copyright (c) 2015年 LiCQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
